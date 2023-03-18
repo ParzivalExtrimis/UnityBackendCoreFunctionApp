408251237f1e089eb1e385e0a2949cbeb9156791
ParzivalExtrimis <hatlermiles69@gmail.com>
2023-03-18 19:30:39 +0530

    string output instead of Action Response

--- UnityBackendCoreFunctionApp/Functions/CoreFunction.cs
+++ UnityBackendCoreFunctionApp/Functions/CoreFunction.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.DurableTask;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Schema;
 using UnityBackendCoreFunctionApp.Models;
 using static System.Reflection.Metadata.BlobBuilder;
@@ -15,7 +16,7 @@ namespace UnityBackendCoreFunctionApp.Functions;
 public static class CoreFunction {
 
     [FunctionName("Core")]
-    public static async Task<ActionResult> Core(
+    public static async Task<string> Core(
         [OrchestrationTrigger] IDurableOrchestrationContext context,
         ILogger log) {
 
@@ -27,17 +28,16 @@ public static class CoreFunction {
         var jsonContent = context.GetInput<string>();
         var userData = await context.CallActivityWithRetryAsync<User>("Auth", retryOptions, jsonContent);
 
-        log.LogWarning($"Core: Authenticated -- {userData.Email}");
-
         try {
-            Result result = new Result();
-            var storageInitResult = await context.CallActivityWithRetryAsync<string>("StorageInit", retryOptions, userData.Email);
-            var contentMatchResult = await context.CallActivityAsync<ContentData>("ContentMatcher", userData);
-
-            result.location = storageInitResult;
-            result.data = contentMatchResult;
-
             if (userData != null) {
+                Result result = new Result();
+                var storageInitResult = await context.CallActivityWithRetryAsync<string>("StorageInit", retryOptions, userData.Email);
+                var contentMatchResult = await context.CallActivityAsync<ContentData>("ContentMatcher", userData);
+
+                result.location = storageInitResult;
+                result.data = contentMatchResult;
+                log.LogWarning($"Core: Authenticated -- {userData.Email}");
+
                 var init_match_results = new List<Task<bool>> {
                     storageInitResult != null
                     ? Task<bool>.FromResult(true)
@@ -61,13 +61,13 @@ public static class CoreFunction {
                     }
                 }
                 log.LogWarning($"Core Executed Successfully -- ({userData.Email})");
-                return new OkObjectResult(result);
+                return JsonConvert.SerializeObject(result);
             }
-            return new BadRequestObjectResult($"User could not be authenticated.");
+            return new BadRequestObjectResult($"User could not be authenticated.").ToString();
         }
         catch (Exception ex) {
             log.LogError(ex.Message);
-            return new BadRequestObjectResult("Exception thrown during execution. Something went wrong.");
+            return new BadRequestObjectResult("Exception thrown during execution. Something went wrong.").ToString();
         }
     }
 
